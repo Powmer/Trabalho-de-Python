33c90d3f8d306bcccdf9e62c1c00e7d235728efd
DanDan2804 <danieltavares2010@gmail.com>
2025-06-04 21:36:56 -0300

    caractere removido (colchete)

--- app.py
+++ app.py
@@ -203,7 +203,6 @@ def abrir_planilha():
 janela = tk.Tk()
 janela.title("Cadastro e Login de Email")
 janela.geometry("600x400")
-]
 img = PhotoImage(file="Baking-Bread-Logo.png")
 label_titulo = tk.Label(janela, image=img)
 label_titulo.pack(pady=10)
